78a3f2ed342c7e3b439ef7016970871cdd157591
zhangyue <423257356@qq.com>
2019-10-22 17:06:28 +0800

    请求类单例，删除测试文件

--- test/test-accesstoken.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once dirname(__DIR__) . "/vendor/autoload.php";
-
-use zyblog\wxMpCloudHttpApi\AccessToken;
-
-print_r(AccessToken::getWxAccessToken('', ''));
--- test/test-config.php
+++
@@ -1,3 +0,0 @@
-<?php
-$env = '';
-$accessToken = '';
